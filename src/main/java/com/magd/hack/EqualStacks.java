2ae7f13b38d7db261d40b90db061f31dceaebb2a
Ali <mailofmajidi@gmail.com>
2021-12-15 00:38:59 +0000

    commit message to ali

--- src/main/java/com/magd/hack/EqualStacks.java
+++ src/main/java/com/magd/hack/EqualStacks.java
@@ -4,7 +4,7 @@ package com.magd.hack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -54,8 +54,29 @@ public class EqualStacks {
     }
     public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3){
         // Write your code here
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
 
+        int[] sum = new int[3];
+        map.put(0, h1);
+        map.put(1, h2);
+        map.put(2, h3);
 
-        return 0;
+        sum[0] = h1.stream().mapToInt(integer -> integer).sum();
+        sum[1] = h2.stream().mapToInt(integer -> integer).sum();
+        sum[2] = h3.stream().mapToInt(integer -> integer).sum();
+
+        int best, j;
+
+        while (!((sum[0] == sum[1]) && (sum[1] == sum[2]))) {
+            best = Math.max(Math.max(sum[0], sum[1]), sum[2]);
+            if (best == sum[0])
+                j = 0;
+            else if (best == sum[1])
+                j = 1;
+            else j = 2;
+          sum[j] -= map.get(j).get(0);
+          map.get(j).remove(0);
+        }
+        return sum[0];
     }
 }
